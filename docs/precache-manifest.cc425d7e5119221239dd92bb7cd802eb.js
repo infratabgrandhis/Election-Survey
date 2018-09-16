8e099fc431d4b673eb60442732c97227c590bc04
Grandhi <grandhi.subrahmanyam@applaudsolutions.com>
2018-09-16 16:38:55 +0530

    included pwa into app

---
+++ docs/precache-manifest.cc425d7e5119221239dd92bb7cd802eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/electionsurvey/robots.txt"
+  },
+  {
+    "revision": "cd202a9bff0d063274d2",
+    "url": "/electionsurvey/js/chunk-vendors.ba6fefdc.js"
+  },
+  {
+    "revision": "688b07cfa516e93226c3",
+    "url": "/electionsurvey/js/app.9d1d4922.js"
+  },
+  {
+    "revision": "a1dc1c90b28cdd42debf95438dadb24d",
+    "url": "/electionsurvey/index.html"
+  },
+  {
+    "revision": "cd202a9bff0d063274d2",
+    "url": "/electionsurvey/css/chunk-vendors.0c3e5376.css"
+  },
+  {
+    "revision": "688b07cfa516e93226c3",
+    "url": "/electionsurvey/css/app.b55bd235.css"
+  }
+];
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import store from './store'
 import router from './router'
 import App from './App.vue'
 import firebaseConfig from './config/firebase.json'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
