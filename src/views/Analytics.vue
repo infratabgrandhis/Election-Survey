a3d0907164d4bfab2040a7b70427ef17d100c348
Grandhi <grandhi.subrahmanyam@applaudsolutions.com>
2018-09-29 16:06:28 +0530

    made chart as Dynamic

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -9,14 +9,18 @@
                               v-model="constituency"
                               class="full-width"
                               label="constituencies"></v-select>
-                    <v-card>
                         <v-layout row
                             wrap
                             align-center>
-                        <p v-if="!constituency">Before see Analytics, Select constituency from the dropdown</p>
-                        <DummyChart :constituencyName="constituency" v-else></DummyChart>
+                        <v-card v-if="!constituency">
+                          <p >Before see Analytics, Select constituency from the dropdown</p>
+                        </v-card>
+                        <div v-else>
+                          <Chart :data="getChartData('Rvdglq55qaFYphTZQjia6Etv')" title="Parties status based on constituency"></Chart>
+                          <v-divider dark inset></v-divider>
+                          <!-- <Chart :data="getChartData('Rvdglq55qaFYphTZQjia6Etv')" title="Parties status based on constituency"></Chart> -->
+                        </div>
                         </v-layout>
-                    </v-card>
                 </v-layout>
             </v-container>
         </v-form>
@@ -26,33 +30,65 @@
 <script>
 import { db } from "@/fire.js";
 import util from "@/util/util.js";
-import _values from "lodash/values";
-import DummyChart from "./DummyChart.vue";
+import _groupBy from "lodash/groupBy";
+import _map from "lodash/map";
+import Chart from "./DummyChart.vue";
 
 export default {
   data() {
     return {
       constituenciesList: [],
-      constituency: ""
+      constituency: "",
+      resultSet: []
     };
   },
   components: {
-    DummyChart
+    Chart
   },
   methods: {
     getConstituenciesList() {
       db.collection("ConstituencyList")
         .get()
         .then(result => {
-          this.constituenciesList = util.firebaseGetValidator(result).data.sort();
+          this.constituenciesList = util
+            .firebaseGetValidator(result)
+            .data.sort();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    getChartData(queId) {
+      let data = _groupBy(this.resultSet, item => {
+        return item[queId];
+      });
+      data = _map(data, (value, key) => {
+        return {
+            name: key,
+            y: value.length
+        }
+      })
+      console.log(data);
+      return data;
+    }
+  },
+  created() {
+    this.getConstituenciesList();
+  },
+  watch: {
+    constituency(newValue, oldValue) {
+      db.collection("Feedback")
+        .where("constituency", "==", newValue)
+        .get()
+        .then(result => {
+          this.resultSet = result.docs.map(doc => {
+            return doc.data();
+          });
         })
         .catch(err => {
           console.log(err);
         });
     }
-  },
-  created() {
-    this.getConstituenciesList();
   }
 };
 </script>
--- src/views/DummyChart.vue
+++ src/views/DummyChart.vue
@@ -1,103 +1,60 @@
 <template>
-    <highcharts :key="constituencyName" :options="chartOptions"></highcharts>
+    <v-card>
+      <highcharts :key="data" :options="chartOptions"></highcharts>
+    </v-card>
 </template>
 
 <script>
-import {Chart} from 'highcharts-vue'
+import { Chart } from "highcharts-vue";
 export default {
-    props:{
-        constituencyName:''
+  props: {
+    data: {
+      type: Array,
+      default: []
     },
-    data() {
-        return {
-            chartBasicOptions: {
-                chart: {
-                    type: 'pie',
-                    width: 300,
-                    height: 300,
-                },
-                title: {
-                    text: 'Parties status based on constituency'
-                },
-                plotOptions:{
-                    pie:{
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        showInLegend: true
-                    }
-                },
-                series: [{
-                    name: 'Party'
-                }],
-            },
-            dataList: [
-                [{
-                    name: 'TRS',
-                    y: 51.41,
-                },{
-                    name: 'Congress/TDP/CPI(M)',
-                    y: 30.84
-                }, {
-                    name: 'BJP',
-                    y: 10.85
-                }, {
-                    name: 'Independent',
-                    y: 5
-                }, {
-                    name: 'NOTA',
-                    y: 1.9
-                }],
-                [{
-                    name: 'TRS',
-                    y: 61.41,
-                },{
-                    name: 'Congress/TDP/CPI(M)',
-                    y: 28.84
-                }, {
-                    name: 'BJP',
-                    y: 5.85
-                }, {
-                    name: 'Independent',
-                    y: 3
-                }, {
-                    name: 'NOTA',
-                    y: 0.9
-                }],
-                [{
-                    name: 'TRS',
-                    y: 41.41,
-                },{
-                    name: 'Congress/TDP/CPI(M)',
-                    y: 35.84
-                }, {
-                    name: 'BJP',
-                    y: 15.85
-                }, {
-                    name: 'Independent',
-                    y: 5
-                }, {
-                    name: 'NOTA',
-                    y: 1.9
-                }]
-            ],
-            chartOptions: {}
-        }
-    },
-    mounted() {
-        this.chartBasicOptions.series[0].data = this.dataList[0];
-        this.chartOptions = this.chartBasicOptions;
-    },
-    components: {
-        highcharts: Chart 
-    },
-    watch: {
-        constituencyName(newValue, oldValue) {
-            let tempVal = Math.floor(Math.random() * 3) + 1;
-            this.chartBasicOptions.series[0].data = this.dataList[tempVal-1];
-            this.chartOptions = this.chartBasicOptions;
+    title: ""
+  },
+  data() {
+    return {
+      chartBasicOptions: {
+        chart: {
+          type: "pie",
+          width: 300,
+          height: 300
         },
-    },
-}
+        title: {},
+        plotOptions: {
+          pie: {
+            dataLabels: {
+              enabled: false
+            },
+            showInLegend: true
+          }
+        },
+        series: [
+          {
+            name: "Party"
+          }
+        ]
+      },
+      chartOptions: {}
+    };
+  },
+  mounted() {
+    this.chartBasicOptions.title.text = this.title;
+    this.chartBasicOptions.series[0].data = this.data;
+    this.chartOptions = this.chartBasicOptions;
+  },
+  components: {
+    highcharts: Chart
+  },
+  watch: {
+    data(newValue, oldValue) {
+      this.chartBasicOptions.title.text = this.title;
+      this.chartBasicOptions.series[0].data = this.data;
+      this.chartOptions = this.chartBasicOptions;
+    }
+  }
+};
 </script>
 
